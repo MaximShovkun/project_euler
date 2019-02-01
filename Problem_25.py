7c5d02f1e10a258fb201446cc1506e49adf8e57f
MaximShovkun <maximshovkun95@gmail.com>
2019-02-01 16:37:23 +0200

    Update Problem_25.py

--- Problem_25.py
+++ Problem_25.py
@@ -1,16 +1,20 @@
-def better_fib(n):
-    if n == 0 or n == 1:
-        return n
-    else:
-        fib = [0, 1]
-        for i in range(2, n + 1):
-            fib.append(fib[i - 1] + fib[i - 2])
-        return fib[n]
+import time
+start_time = time.time()
 
-    
+
+def better_fib(n):
+    fib = [0, 1]
+    for i in range(2, n + 1):
+        fib.append(fib[i - 1] + fib[i - 2])
+    return fib[n]
+
+
 i = 0
 while True:
     i += 1
     if len(str(better_fib(i))) == 1000:
         print(i)
         break
+
+
+print ("time elapsed: {:.2f}s".format(time.time() - start_time))
